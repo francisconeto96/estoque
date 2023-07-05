79ee37a6cc8eb8403cd3abe74d2d788a6af6ed32
Francisco Neto <98923737+francisconeto96@users.noreply.github.com>
2023-07-05 20:24:36 -0300

    Add files via upload

---
+++ painel.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYvov66CiqBlKUQfGQb4FdIa2nZ6s-Si8",
+    authDomain: "estoque-876f2.firebaseapp.com",
+    projectId: "estoque-876f2",
+    storageBucket: "estoque-876f2.appspot.com",
+    messagingSenderId: "48035127557",
+    appId: "1:48035127557:web:6133d76f54cc9af160576c",
+    measurementId: "G-WRRZRQZSPY"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  
